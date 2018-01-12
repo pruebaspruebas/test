99393eb22430b5dcbb9cf75ff614b25f08206662
alf <a@a.com>
2018-01-12 12:39:38 +0100

    al repo github

--- app/src/main/java/com/alf/org/repo/MainActivity.java
+++ app/src/main/java/com/alf/org/repo/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //comentario del V1
+        //Subo al repo github
     }
 }
