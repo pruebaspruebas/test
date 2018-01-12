ede495c0213c6c71cf972e912cf64f03f443c012
alf <a@a.com>
2018-01-12 10:27:20 +0100

    text v1

--- app/src/main/java/com/alf/org/repo/MainActivity.java
+++ app/src/main/java/com/alf/org/repo/MainActivity.java
@@ -7,7 +7,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //cambios 2
+        //Branch V1
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
